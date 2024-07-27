570a377da9bbb3fd4d1f01a0f20947f1bfda1b60
Alejandro <alejandro.gutierrez01@epn.edu.ec>
2024-07-26 22:51:02 -0500

    Tercer commit

--- src/Login.java
+++ src/Login.java
@@ -1,12 +1,13 @@
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Arrays;
 
 public class Login {
     public JPanel loginPane;
     private JTextField userText;
     private JPasswordField contraseniaText;
-    private JComboBox comboBox1;
+    private JComboBox<String> comboBox1;
     private JButton enviarButton;
     private JButton registrateButton;
 
@@ -14,15 +15,37 @@ public class Login {
         enviarButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                JFrame frame = new JFrame();
-                frame.setTitle("Gestión de usuarios");
-                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-                frame.setContentPane(new Gestion().gestionPanel);
-                frame.pack();
-                frame.setSize(800, 600);
-                frame.setVisible(true);
-                frame.setLocationRelativeTo(null);
-                ((JFrame) SwingUtilities.getWindowAncestor(enviarButton)).dispose();
+                String item = (String) comboBox1.getSelectedItem();
+                String user = userText.getText();
+                String password = Arrays.toString(contraseniaText.getPassword());
+
+                if ( !user.isEmpty() && !password.isEmpty() && !"Seleccione una opción".equals(item) ){
+                    if("Administrador".equals(item)){
+                        JFrame frame = new JFrame();
+                        frame.setTitle("Gestión de usuarios");
+                        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                        frame.setContentPane(new Gestion().gestionPanel);
+                        frame.pack();
+                        frame.setSize(800, 600);
+                        frame.setVisible(true);
+                        frame.setLocationRelativeTo(null);
+                        ((JFrame) SwingUtilities.getWindowAncestor(enviarButton)).dispose();
+                    }
+                    else if("Encargado/Jugador".equals(item)){
+                        JFrame frame = new JFrame();
+                        frame.setTitle("Gestión de usuarios");
+                        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                        frame.setContentPane(new Reserva().reservaPanel);
+                        frame.pack();
+                        frame.setSize(800, 600);
+                        frame.setVisible(true);
+                        frame.setLocationRelativeTo(null);
+                        ((JFrame) SwingUtilities.getWindowAncestor(enviarButton)).dispose();
+                    }
+                }
+                else {
+                    JOptionPane.showInputDialog(null,"sexo");
+                }
             }
         });
         registrateButton.addActionListener(new ActionListener() {
@@ -37,6 +60,11 @@ public class Login {
                 frame.setVisible(true);
                 frame.setLocationRelativeTo(null);
                 ((JFrame) SwingUtilities.getWindowAncestor(enviarButton)).dispose();
+            }
+        });
+        comboBox1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
             }
         });
     }
---
+++ src/Reserva.java
@@ -0,0 +1,6 @@
+import javax.swing.*;
+
+public class Reserva {
+    public JPanel reservaPanel;
+    private JTable table1;
+}
